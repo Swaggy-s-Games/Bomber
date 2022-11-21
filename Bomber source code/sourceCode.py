b7adbfcc40946233db32f5564d745b85f1110c08
ItsSwaggy <113780379+ItsSwaggy@users.noreply.github.com>
2022-11-21 09:23:50 +0100

    Add files via upload

---
+++ Bomber source code/sourceCode.py
@@ -0,0 +1,116 @@
+import keyboard
+import time
+
+def main():
+    print()
+    print()
+    print()
+    print()
+    print(" ________  ___       __   ________  ________  ________      ___    ___ ________         ")
+    print("|\   ____\|\  \     |\  \|\   __  \|\   ____\|\   ____\    |\  \  /  /|\   ____\        ")
+    print("\ \  \___|\ \  \    \ \  \ \  \|\  \ \  \___|\ \  \___|    \ \  \/  / | \  \___|_       ")
+    print(" \ \_____  \ \  \  __\ \  \ \   __  \ \  \  __\ \  \  ___   \ \    / / \ \_____  \      ")
+    print("  \|____|\  \ \  \|\__\_\  \ \  \ \  \ \  \|\  \ \  \|\  \   \/  /  /   \|____|\  \     ")
+    print("    ____\_\  \ \____________\ \__\ \__\ \_______\ \_______\__/  / /       ____\_\  \    ")
+    print("   |\_________\|____________|\|__|\|__|\|_______|\|_______|\___/ /       |\_________\   ")
+    print("   \|_________|                                           \|___|/        \|_________|   ")
+    print("                                                                                        ")
+    print("                                                                                        ")
+    print(" ________  ________  _____ ______   ________  _______   ________     ")
+    print("|\   __  \|\   __  \|\   _ \  _   \|\   __  \|\  ___ \ |\   __  \    ")
+    print("\ \  \|\ /\ \  \|\  \ \  \ \__\ \  \ \  \|\ /\ \   __/|\ \  \|\  \   ")
+    print(" \ \   __  \ \  \|\  \ \  \ |__| \  \ \   __  \ \  \_|/_\ \   _  _\  ")
+    print("  \ \  \|\  \ \  \|\  \ \  \    \ \  \ \  \|\  \ \  \_|\ \ \  \\   \ ")
+    print("   \ \_______\ \_______\ \__\    \ \__\ \_______\ \_______\ \__\\  _\ ")
+    print("    \|_______|\|_______|\|__|     \|__|\|_______|\|_______|\|__|\|__|")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("Type RR to spam Rick rolls")
+    print("Type sussy to spam the rock")
+    print()
+    user_input = input("What do you wanna spam? ")
+    print()
+    if user_input.lower() == "rr":
+        user_input = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    if user_input.lower() == "sussy":
+        user_input = "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠛⠛⠛⠛⠿⣿⣿⣿⣿⣿⣿⣿⣿\
+                    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀⠉⠻⣿⣿⣿⣿⣿⣿\
+                    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢿⣿⣿⣿⣿\
+                    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⣿\
+                    ⣿⣿⣿⣿⣿⣿⣿⠋⠈⠀⠀⠀⠀⠐⠺⣖⢄⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿\
+                    ⣿⣿⣿⣿⣿⣿⡏⢀⡆⠀⠀⠀⢋⣭⣽⡚⢮⣲⠆⠀⠀⠀⠀⠀⠀⢹⣿⣿⣿⣿\
+                    ⣿⣿⣿⣿⣿⣿⡇⡼⠀⠀⠀⠀⠈⠻⣅⣨⠇⠈⠀⠰⣀⣀⣀⡀⠀⢸⣿⣿⣿⣿\
+                    ⣿⣿⣿⣿⣿⣿⡇⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣟⢷⣶⠶⣃⢀⣿⣿⣿⣿⣿\
+                    ⣿⣿⣿⣿⣿⣿⡅⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⠀⠈⠓⠚⢸⣿⣿⣿⣿⣿\
+                    ⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⢀⡠⠀⡄⣀⠀⠀⠀⢻⠀⠀⠀⣠⣿⣿⣿⣿⣿⣿\
+                    ⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠐⠉⠀⠀⠙⠉⠀⠠⡶⣸⠁⠀⣠⣿⣿⣿⣿⣿⣿⣿\
+                    ⣿⣿⣿⣿⣿⣿⣿⣦⡆⠀⠐⠒⠢⢤⣀⡰⠁⠇⠈⠘⢶⣿⣿⣿⣿⣿⣿⣿⣿⣿\
+                    ⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠠⣄⣉⣙⡉⠓⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿\
+                    ⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\
+                    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣤⣀⣀⠀⣀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿"
+    amount = input("How much do you want to spam? ")
+    print()
+
+    counter = -1
+
+    check = True
+    check2 = False
+    check4 = False
+    check5 = False
+    check6 = False
+    check3 = False
+
+
+
+    user = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+
+    running = True
+    while running:
+
+        if counter == -1:
+            check2 = True
+            if check2 == True and check == True:
+                print("Press W to start")
+                check = False
+            if keyboard.read_key() == "w":
+                keyboard.press_and_release("Delete")
+                check = True
+                check2 = False
+                counter += 1
+
+        if counter > -1 and counter < int(amount):
+            keyboard.write(user_input)
+            keyboard.press_and_release("enter")
+            counter += 1
+
+        if counter == int(amount):
+            time.sleep(2)
+            counter += 1
+
+        if counter > int(amount) and check5 == False:
+            thanks = "Thanks for using the spammer:D"
+            print()
+            print()
+            print()
+            print(thanks)
+            print()
+            print()
+            print()
+            print("Made by Swaggy")
+            print()
+            print()
+            print("Press E to restart!")
+            print("or")
+            print("Press Q to quit!")
+            check5 = True
+
+        if counter > int(amount) and keyboard.read_key() == "q":
+            quit()
+        
+        if counter > int(amount) and keyboard.read_key() == "e":
+            main()
+
+main()
